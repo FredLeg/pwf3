928fb8d7f766703f927a8eb45eed6367a8364417
banh <manbanhduc@gmail.com>
2015-07-27 15:53:41 +0200

    ajout le chemin de css dans TrombinoController

--- controllers/TrombinoController.class.php
+++ controllers/TrombinoController.class.php
@@ -8,6 +8,9 @@ class TrombinoController extends BaseController {
     $vars = array(
       'title' => 'Trombinoscope wf3',
       'description' => 'Les étudinats de votre groupe',
+      'url_photos' => IMG_HTTP. '/trombino/10',
+      'pwf3Css' => CSS_HTTP. 'pwf3.css',
+
     );
 
     $this->render('trombino', $vars);
